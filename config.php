dbb64972f252b60338fa6f890f16d303225e089f
Renato Galvão <renatopgalvao@gmail.com>
2018-04-16 17:55:19 -0300

    Add files via upload

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-
+	
 	session_start();
-	date_default_timezone_set('America/Sao_Paulo');
+	date_default_timezone_set("America/Sao_Paulo");
 	$autoload = function($class){
-		if($class == 'Email'){
+		if ($class == 'Email'){
 			require_once('classes/phpmailer/PHPMailerAutoload.php');
 		}
 		include('classes/'.$class.'.php');
@@ -11,55 +11,18 @@
 
 	spl_autoload_register($autoload);
 
-
-	define('INCLUDE_PATH','http://localhost/projeto_01_servidor/');
-	define('INCLUDE_PATH_PAINEL',INCLUDE_PATH.'painel/');
-
-	define('BASE_DIR_PAINEL',__DIR__.'/painel');
-
-
-	//Conectar com banco de dados!
+	define('BASE_URL', 'http://localhost/danki/dev_web/Projeto_01/');
+	define('BASE_PAINEL', 'http://localhost/danki/dev_web/Projeto_01/painel/');
+	define('BASE_DIR_PANEL', __DIR__.'/painel');
+	
+	//Constantes da conexão com o banco de dados
 	define('HOST','localhost');
-	define('USER','root');
-	define('PASSWORD','');
 	define('DATABASE','projeto_01');
-
-	//Constantes para o painel de controle
-	define('NOME_EMPRESA','Danki Code');
-
-	//Funções do painel
-	function pegaCargo($indice){
-		return Painel::$cargos[$indice];
-	}
-
-	function selecionadoMenu($par){
-		/*<i class="fa fa-angle-double-right" aria-hidden="true"></i>*/
-		$url = explode('/',@$_GET['url'])[0];
-		if($url == $par){
-			echo 'class="menu-active"';
-		}
-	}
-
-	function verificaPermissaoMenu($permissao){
-		if($_SESSION['cargo'] >= $permissao){
-			return;
-		}else{
-			echo 'style="display:none;"';
-		}
-	}
-
-	function verificaPermissaoPagina($permissao){
-		if($_SESSION['cargo'] >= $permissao){
-			return;
-		}else{
-			include('painel/pages/permissao_negada.php');
-			die();
-		}
-	}
+	define('USER','root');
+	define('PASS','root');
 
 	function recoverPost($post){
 		if(isset($_POST[$post])){
 			echo $_POST[$post];
 		}
 	}
-?>
